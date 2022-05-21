f4791b1ae2628f0877e6fd2fc8225a7fcb17b26f
parlinduha <dhparlin@gmail.com>
2022-05-21 17:28:34 +0700

    melakukan perubahan di file controller.js

--- controller.js
+++ controller.js
@@ -4,5 +4,5 @@ var response = require('./res');
 var connection = require('./koneksi');
 
 exports.index = function(req, res){
-    response.ok("Application rest api is running");
+    response.ok("Application rest api is running",res);
 }
